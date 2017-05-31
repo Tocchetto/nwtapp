99adc77e40b1d0b0ed1febf854e0314c66f65d2c
AngelaMazzonetto <144981@upf.br>
2017-05-31 17:00:15 -0300

    variaveis adicionadas, modelos, meses

--- ProjetoVisualiza/global.R
+++ ProjetoVisualiza/global.R
@@ -19,8 +19,27 @@ lat = -14.940753
 #Então toy_list terá 2 vetores, um com as estações e outro com os mêses: toy_list$Season e toy_list$Month
 #list(Season = ("Inverno", "Primavera", "Verão", "Outono"), Month = ("Jan" "Fev" "Mar" "Abr" "Maio" "Jun" "Jul" "Aug" "Set" "Out" "Nov" "Dez")) -> isso nao funciona...
 season.labels <- c("Inverno","Primavera","Verão","Outono")
-toy_list <- list(Season=season.labels, Month=month.abb)
+toy_list <- list(Season=season.labels)
+#month_list <- list(Month=month.abb)
+month_list = c("Jan","Fev","Mar","Abr","Maio","Jun","Jul","Aug","Set","Out","Nov","Dez")
+
+modelo = c("ETA_MIROC5_RCP4.5_20KM","ETA_MIROC5_RCP8.5_20KM","ETA_HADGEM2-ES_RCP4.5_20KM","ETA_HADGEM2-ES_RCP4.5_5KM","ETA_HADGEM2-ES_RCP8.5_20KM","ETA_HADGEM2-ES_RCP8.5_5KM","ETA_CANESM2-ES_RCP4.5_20KM","ETA_CANESM2-ES_RCP8.5_20KM","ETA_BESM-ES_RCP4.5_20KM","ETA_BESM-ES_RCP8.5_20KM")
 
 #Lista das Variáveis
-var.labels <- c("Temperatura", "Pressão", "Umidade")
+var.labels <- c("Pressão ao Nível Médio do Mar", "Pressão à Superfície", "Componente Meridional do Vento a 10 m da Superfície", 
+                "Componente Zonal do Vento a 10 m da Superfície", "Temperatura a 2 m da Superfície", "Temperatura Máxima", 
+                "Temperatura Mínima", "Temperatura do Ponto de Orvalho", "Componente Zonal do Vento a 100 m da Superfície", 
+                "Componente Meridional do Vento a 100 m da Superfície", "Precipitação Total", "Precipitação Convectiva",
+                "Precipitação Estratiforme", "Neve", "Fluxo de Calor Latente à Superfície", "Fluxo de Calor Sensível à Superfície", 
+                "Fluxo de Calor no Solo", "Temperatura à Superfície", "Temperatura do Solo na Camada de 0-10 cm da Superfície", 
+                "Temperatura do Solo na Camada de 10-40 cm da Superfície", "Umidade do Solo na Camada de 0-10 cm da Superfície", 
+                "Umidade do Solo na Camada de 10-40 cm da Superfície", "Conteúdo de Água Disponível no Solo", "Escoamento Superficial", 
+                "Escoamento Subsuperficial", "Evapotranspiração Potencial à Superfície", "Cobertura de Nuvens Baixas", 
+                "Cobertura de Nuvens Médias", "Cobertura de Nuvens Altas", "Radiação de Onda Curta Incidente à Superfície", 
+                "Radiação de Onda Longa Incidente à Superfície", "Radiação de Onda Curta Emergente à Superfície", 
+                "Radiação de Onda Longa Emergente à Superfície", "Radiação de Onda Curta Emergente no Topo da Atmosfera",
+                "Radiação de Onda Longa Emergente no Topo da Atmosfera", "Albedo Superficial", "Energia Potencial Disponível para Convecção",
+                "Água Precipitável", "Altura Geopotencial", "Componente Zonal do Vento", "Componente Meridional do Vento", "Temperatura Absoluta",
+                "Umidade Relativa", "Movimento Vertical", "Umidade Específica")
+
 cities.labels <- c("Porto Alegre", "Passo Fundo")
